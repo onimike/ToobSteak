364f839273198b3ad40eab59b3b8241f2e685cda
Alex Bukin <abukin@csu.fullerton.edu>
2012-03-11 13:36:44 -0700

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,8 @@
+#### CPSC473 Spring 2012
+##### Team 1 - require 'rails'
+
++ Aleksey Bukin
++ Iris Markofsky
++ Michael Robertson
++ Michael Romain
++ Yuta Takayama
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToobSteak::Application.config.secret_token = '80b93c334f331f2dc7c264339e0ad4573ed630297c113e5af301e551c92f01822c586cae910756d8135f1547fcb9347cf42cf289dd692474b27d81080ed415ab'
