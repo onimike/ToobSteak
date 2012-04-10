024a72c715f8a5dbd0df8e822e4ab2a19bf583da
Michael Robertson <mirob2005@gmail.com>
2012-04-10 12:43:27 -0700

    Added Contact,Help, added matches to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ ToobSteak::Application.routes.draw do
   match '/newshow', :to => 'shows#new'
   match '/about',	:to => 'static_pages#about'
   match '/signup',  :to => 'users#new'
+  match '/contact', :to => 'static_pages#contact'
+  match '/help',    :to => 'static_pages#help
 
   #/
   root :to => 'static_pages#home'
