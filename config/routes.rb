430fa40ef9db68b118c00ec61434b23949757617
Aleksey Bukin <abukin@csu.fullerton.edu>
2012-04-10 12:36:52 -0700

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,28 @@
 ToobSteak::Application.routes.draw do
   resources :users, :shows
   resources :sessions, :only => [:new, :create, :destroy]
+  
+  #static pages
+  get "static_pages/about"
 
+
+  #users
+  get "users/new"
   get "users/create"
   get "users/edit"
   get "users/update"
   get "users/destroy"
-
   get "users/show"
+  
+  #shows
   get "shows/new"
-  get "users/new"
-  get "static_pages/about"
 
-  resources :users, :shows
-
+  #routes
   match '/newshow', :to => 'shows#new'
-
   match '/about',	:to => 'static_pages#about'
   match '/signup',  :to => 'users#new'
 
+  #/
   root :to => 'static_pages#home'
 
 end
