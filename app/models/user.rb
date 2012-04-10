90af470e7f94175fccb9d652d1bd4aab2ca5b62f
Aleksey Bukin <abukin@csu.fullerton.edu>
2012-04-10 12:42:48 -0700

    added authentication methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,37 @@ class User < ActiveRecord::Base
 						:length   => { :maximum => 50 }
 
 	validates :email,	:presence => true
-						:formate  => { :with => email_regex }
+						:format  => { :with => email_regex }
 						:uniqueness => { :case_sensitive => false }
 						:length       => { :within => 8..40 }
+
+	validates :password, :presence     => true,
+                         :confirmation => true,
+                         :length       => { :within => 6..40 }
+
+    before_save :encrypt_password
+
+	def has_password?(submitted_password)
+    	encrypted_password == encrypt(submitted_password)
+  	end
+
+    private
+
+		def encrypt_password
+		  self.salt = make_salt unless has_password?(password)
+		  self.encrypted_password = encrypt(password)
+		end
+
+		def encrypt(string)
+		  secure_hash("#{salt}--#{string}")
+		end
+
+		def make_salt
+		  secure_hash("#{Time.now.utc}--#{password}")
+		end
+
+		def secure_hash(string)
+		  Digest::SHA2.hexdigest(string)
+		end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120410191227) do
+ActiveRecord::Schema.define(:version => 20120410194042) do
 
   create_table "shows", :force => true do |t|
     t.string   "name"
@@ -20,11 +20,15 @@ ActiveRecord::Schema.define(:version => 20120410191227) do
     t.datetime "updated_at"
   end
 
+  add_index "shows", ["name"], :name => "index_shows_on_name", :unique => true
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
