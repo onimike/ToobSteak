b12a064bbce478fb72a0f22e6c406ad8d323fc03
Aleksey Bukin <abukin@csu.fullerton.edu>
2012-04-10 12:34:04 -0700

    fixed routes.rb (again)

--- config/routes.rb
+++ config/routes.rb
@@ -11,23 +11,14 @@ ToobSteak::Application.routes.draw do
   get "shows/new"
   get "users/new"
   get "static_pages/about"
-<<<<<<< HEAD
 
   resources :users, :shows
 
-=======
-  
->>>>>>> 1fc62d1ad57cbb7ba6c278dd328eb42497706389
   match '/newshow', :to => 'shows#new'
 
-  root :to => 'pages#home'
-<<<<<<< HEAD
-=======
-  
->>>>>>> 1fc62d1ad57cbb7ba6c278dd328eb42497706389
-
-  #named routes
   match '/about',	:to => 'static_pages#about'
   match '/signup',  :to => 'users#new'
+
+  root :to => 'static_pages#home'
 
 end
