80adce2b563d0c1eeeb753da3eb36292f710a173
Aleksey Bukin <abukin@csu.fullerton.edu>
2012-04-17 11:53:10 -0700

    fixed(?) pagination

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,25 +1,25 @@
 require 'tvdb_party' #documentation: http://rubydoc.info/gems/tvdb_party/0.6.2/frames
 
 class ShowsController < ApplicationController
-	
+
 	def show
 		@show = Show.find(params[:id])
 	end
-	
+
 	 def index
-    @shows = Show.paginate(:page => 10)
+    @shows = Show.paginate(:page => params[:page], :per_page => 10)
   end
-	
+
 	def new
 		@show = Show.new
 	end
-	
+
 	def create
 		tvdb = TvdbParty::Search.new("F6EA8F2FD26C08BF")
 		@show = Show.new(params[:show])
 		@results = tvdb.search(@show.name)
 		@seriesid = ""
-		
+
 		@results.each_index do |res|
 			if @results[res]["SeriesName"].upcase == @show.name.upcase #This matches the show name exactly
 
@@ -27,7 +27,7 @@ class ShowsController < ApplicationController
 				@show.description = @results[res]["Overview"]
 #				@show.banner = @results[res]["banner"]
 				@show.seriesid = @results[res]["seriesid"]
-				
+
 				if @show.save #only saves if there are no duplicates
 #					@show.delay.fill @results[res]["seriesid"] #this only works if you have a computer doing rake jobs:work
 					@show.fill @show.seriesid
@@ -38,8 +38,8 @@ class ShowsController < ApplicationController
 				end
 			end
 		end
-		
+
 		render 'error'
-		
+
 	end
 end
