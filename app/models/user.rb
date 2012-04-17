29aa87b8188f348b98945e9c73c6a0c5ba597224
Aleksey Bukin <abukin@csu.fullerton.edu>
2012-04-17 12:20:32 -0700

    added following and follow  methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
-def self.authenticate(email, submitted_password)
+  def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
@@ -47,6 +47,14 @@ def self.authenticate(email, submitted_password)
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
   end
 
   private
