e971804764896ee2974d8cc3e67fb3ea851f796b
Aleksey Bukin <abukin@csu.fullerton.edu>
2012-04-17 12:23:24 -0700

    added reverse relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,9 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
 
-
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
   has_many :relationships, :foreign_key => "follower_id",
                            :dependent => :destroy
   has_many :following, :through => :relationships, :source => :followed
